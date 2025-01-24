d97dd5f0c2961b889e4f138e3ad5311f002759eb
Bilo <mateojimenezfamaart@gmail.com>
2025-01-24 07:45:10 -0500

    Clase 1 done

--- Assets/Sesiones/MATEO JIMENEZ/Testings/VisualizerTutorial/AudioPeer.cs
+++ Assets/Sesiones/MATEO JIMENEZ/Testings/VisualizerTutorial/AudioPeer.cs
@@ -23,4 +23,39 @@ public class AudioPeer : MonoBehaviour
     {
         _audioSource.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
     }
+
+    void MakeFrequencyBands()
+    {
+        /*
+         *
+         * 22050 / 512 = 43 Hertz Per Sample
+         * 
+         *20-60 Hz
+         *60-250 Hz
+         *250-500 Hz
+         *2000-4000 Hz
+         *4000-6000 Hz
+         *6000-20000 Hz
+         *
+         * 0 - 2 = 86 Hz 
+         * 1 - 4 = 172 Hz - 87-258
+         * 2 - 8 = 344 Hz - 259-602
+         * 3 - 16 = 688 Hz - 603-1290
+         * 4 - 32 = 1376 Hz - 1291-2666
+         * 5 - 64 = 2752 Hz - 2667-5418
+         * 6 - 128 = 5504 Hz - 5419-10922
+         * 7 - 256 = 11008 Hz - 10923-21930
+         * 510
+         */
+
+        int count = 0;
+
+        for (int i = 0; i < 8; i++)
+        {
+            
+        }
+
+
+    }
+    
 }
